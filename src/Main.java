aef7ab632422496da3cc2c076840f3f209e4e58e
lyays <l28075339@gmail.com>
2025-02-12 22:24:35 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    var dog = 8.0;
+    var cat = 3.6;
+    var paper = 763789;
+     System.out.println(dog);
+     System.out.println(cat);
+     System.out.println(paper);
+    dog = dog + 4.0;
+     System.out.println(dog);
+    cat = cat + 4.0;
+     System.out.println(cat);
+    paper = paper + 4;
+     System.out.println(paper);
+    dog = dog - 3.5;
+     System.out.println(dog);
+    cat = cat - 1.6;
+     System.out.println(cat);
+     paper = paper-7639;
+     System.out.println(paper);
+     var friend = 19;
+     System.out.println(friend);
+     friend = friend + 2;
+     System.out.println(friend);
+     friend = friend/7;
+     System.out.println(friend);
+
     }
 }
