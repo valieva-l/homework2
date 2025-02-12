e6ed5fee3356cd4ecc3ad0ec85af7be811138806
lyays <l28075339@gmail.com>
2025-02-12 22:31:29 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,38 @@ public class Main {
      System.out.println(dog);
      System.out.println(cat);
      System.out.println(paper);
+     //Задача 2
     dog = dog + 4.0;
      System.out.println(dog);
     cat = cat + 4.0;
      System.out.println(cat);
     paper = paper + 4;
      System.out.println(paper);
+        //Задача 3
     dog = dog - 3.5;
      System.out.println(dog);
     cat = cat - 1.6;
      System.out.println(cat);
      paper = paper-7639;
      System.out.println(paper);
+     //Задача 4
      var friend = 19;
      System.out.println(friend);
      friend = friend + 2;
      System.out.println(friend);
      friend = friend/7;
      System.out.println(friend);
+     //Задача 5
      var frog = 3.5;
      System.out.println(frog);
      frog = frog*10;
      System.out.println(frog);
      frog = frog/3.5;
      System.out.println(frog);
+     frog = frog + 4;
+     System.out.println(frog);
+     //Задача 6
+
 
     }
 }
